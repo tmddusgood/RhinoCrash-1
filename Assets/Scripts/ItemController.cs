da8d960a510445a5fc91f6cd8e9a7b79a8139668
tmddusgood <tmddusgood@gmail.com>
2019-06-01 01:40:58 +0900

    Revert "swipe_0.1"
    This reverts commit 83b7da36117da766c067524eb82e16edcb2b0caf.

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    List<GameObject> ItemList = new List<GameObject>();
+    public static int itemcount = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject _obj = Instantiate(Resources.Load("Prefab/item 1")) as GameObject;
+            ItemList.Add(_obj);
+            ItemList[i].transform.localScale = new Vector3(0.5f, 0.5f, 0);
+            ItemList[i].transform.localPosition = new Vector3(-9 + i, 3, 0);
+            ItemList[i].SetActive(false);
+            //ItemList[i].SetActive(true);
+        }
+        Item();
+    }
+
+    public void Item()
+    {
+        for( int i = 0; i < 3; i++)
+        {
+            ItemList[i].SetActive(false);
+        }
+        for (int j = 0; j < itemcount; j++)
+            ItemList[j].SetActive(true);
+    }
+}
