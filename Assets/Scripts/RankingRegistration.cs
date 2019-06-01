329241436decc8142a619badb48c508f70ab4c8e
bhcangel <qusghcsk123@naver.com>
2019-06-01 19:40:39 +0900

    commit

--- Assets/Scripts/RankingRegistration.cs
+++ Assets/Scripts/RankingRegistration.cs
@@ -11,43 +11,71 @@ public class RankingRegistration : MonoBehaviour
     public Text Initial2_Text;
     public Text Initial3_Text;
     public Text Initial_Input_Text;
-    public InputField newname;
+    public Text Name_Text;
+    
+    public static List<string> initial = new List<string>();
 
-    string Name = null;
+    public static int initial_alphabet_index = 0;   //알파벳 리스트에서 지금 몇번째 알파벳인지
+    public static int initial_index = 1;            //지금 몇번째 이니셜을 치고있는지
+    public static string Name;
 
-    List<string> initial = new List<string>();
-
-    public static int initial_alphabet_index = 0;
-
+   
     // Start is called before the first frame update
     void Start()
     {
+
         ScoreText.text = PlayerController.score.ToString();
-        initial.Add("A"); initial.Add("B"); initial.Add("C"); initial.Add("D"); initial.Add("E"); initial.Add("F"); initial.Add("G"); initial.Add("H");
-        initial.Add("I"); initial.Add("J"); initial.Add("K"); initial.Add("L"); initial.Add("M"); initial.Add("N"); initial.Add("O"); initial.Add("P");
-        initial.Add("Q"); initial.Add("R"); initial.Add("S"); initial.Add("T"); initial.Add("U"); initial.Add("V"); initial.Add("W"); initial.Add("X");
-        initial.Add("Y"); initial.Add("Z");
+        //Initial_Input_Text.text = "A";
+
+        initial.Add("A");
+        initial.Add("B");
+        initial.Add("C");
+        initial.Add("D");
+        initial.Add("E");
+        initial.Add("F");
+        initial.Add("G");
+        initial.Add("H");
+        initial.Add("I");
+        initial.Add("J");
+        initial.Add("K");
+        initial.Add("L");
+        initial.Add("M");
+        initial.Add("N");
+        initial.Add("O");
+        initial.Add("P");
+        initial.Add("Q");
+        initial.Add("R");
+        initial.Add("S");
+        initial.Add("T");
+        initial.Add("U");
+        initial.Add("V");
+        initial.Add("W");
+        initial.Add("X");
+        initial.Add("Y");
+        initial.Add("Z");
+
+        Initial_Input_Text.text = initial[initial_alphabet_index].ToString();
+
+
+
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Initial_Input_Text.text = initial[initial_alphabet_index];
-        Initial1_Text.text = initial[initial_alphabet_index];
-        Initial2_Text.text = initial[initial_alphabet_index];
-        Initial3_Text.text = initial[initial_alphabet_index];
-        
-    }
+
 
     public void RightArrow()
     {
+
         RankingRegistration.initial_alphabet_index++;
-        if(RankingRegistration.initial_alphabet_index > 25)
+        if (RankingRegistration.initial_alphabet_index > 25)
         {
             RankingRegistration.initial_alphabet_index -= 26;
-            Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index];
+            Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
         }
-        Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index];
+        Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
+
+
     }
 
     public void LeftArrow()
@@ -56,13 +84,35 @@ public class RankingRegistration : MonoBehaviour
         if (RankingRegistration.initial_alphabet_index < 0)
         {
             RankingRegistration.initial_alphabet_index += 26;
-            Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index];
+            Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
         }
-        Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index];
+        Initial_Input_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
+
     }
 
     public void InitialInput()
     {
-        Name = newname.GetComponent<Text>().ToString();
+        switch (initial_index)
+        {
+            case 1:
+                Initial1_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
+                initial_index++;
+                break;
+            case 2:
+                Initial2_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
+                initial_index++;
+                break;
+            case 3:
+                Initial3_Text.text = initial[RankingRegistration.initial_alphabet_index].ToString();
+
+                Name = Initial1_Text.text.ToString() + Initial2_Text.text.ToString() + Initial3_Text.text.ToString();
+                Name_Text.text = Name;
+                break;
+        }
+
+
+
     }
+
+
 }
