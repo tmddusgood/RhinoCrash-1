700e9fffb2ce85f0ed227ec25db41dac3938e6e8
bhcangel <qusghcks123@naver.com>
2019-06-01 19:08:38 +0900

    commit

---
+++ Assets/Scripts/BackgroundGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGenerator : MonoBehaviour
+{
+    float treeDelta;
+    float weedDelta;
+
+    private void Start()
+    {
+        treeDelta = 0;
+        weedDelta = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.treeDelta += Time.deltaTime;
+        this.weedDelta += Time.deltaTime;
+        if(this.treeDelta > 0.68f)
+        {
+            this.treeDelta = 0;
+            GameObject tree = Instantiate(Resources.Load("Prefab/treePrefab")) as GameObject;
+            if (GameDirector.rand.Next() % 2 == 0)
+                tree.transform.position = new Vector3(-2f, 4.5f, 1);
+            else
+                tree.transform.position = new Vector3(2f, 4.5f, 1);
+            tree.transform.localScale = new Vector3(0.3f, 0.3f, 0);
+        }
+        if(this.weedDelta > 0.4f)
+        {
+            this.weedDelta = 0;
+            GameObject weed = Instantiate(Resources.Load("Prefab/weedPrefab")) as GameObject;
+            if (GameDirector.rand.Next() % 2 == 0)
+                weed.transform.position = new Vector3(-2f, 4.5f, 1);
+            else
+                weed.transform.position = new Vector3(2f, 4.5f, 1);
+            weed.transform.localScale = new Vector3(0.3f, 0.3f, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/SkillController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillController : MonoBehaviour
+{
+    public static bool skillOn = false;
+    public static bool healingOn = false;
+
+    public void Skill()
+    {
+        if (ItemController.itemcount > 0)
+        {
+            skillOn = true;
+            ItemController.itemcount -= 1;
+        }
+    }
+
+    public void Skillhealing()
+    {
+        if (HealingController.healingcount > 0)
+        {
+
+            healingOn = true;
+            HealingController.healingcount -= 1;
+
+        }
+    }
+   
+
+    //public static DateTime Delay(int MS)
+    //{
+    //    DateTime ThisMoment = DateTime.Now;
+    //    TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+    //    DateTime AfterWards = ThisMoment.Add(duration);
+
+    //    while (AfterWards >= ThisMoment)
+    //    {
+    //        System.Windows.Forms.Application.DoEvents();
+    //        ThisMoment = DateTime.Now;
+    //    }
+
+    //    return DateTime.Now;
+    //}
+}
